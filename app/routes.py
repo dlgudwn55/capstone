49f58ae1c343cd01e3d3e222e562f4b5bd539997
LeeHyeongju <a01028245126@gmail.com>
2025-05-19 10:50:18 +0900

    search 검색어 정확도 향상

--- app/routes.py
+++ app/routes.py
@@ -3,9 +3,11 @@ from .db import get_db_connection
 
 main = Blueprint('main', __name__)
 
+
 @main.route('/')
 def index():
-    return  render_template('index.html')
+    return render_template('index.html')
+
 
 @main.route('/search')
 def search():
@@ -14,15 +16,39 @@ def search():
     query = request.args.get('q', '')
     keywords = query.split()
 
-    where_clause = " OR ".join(["keyword1 LIKE ? OR keyword2 LIKE ? OR keyword3 LIKE ?" for _ in keywords])
-    print(where_clause)
-    params = []
-    for kw in keywords:
-        params.extend([kw, kw, kw])
-        conn = get_db_connection()
-        cur = conn.cursor()
+    if len(keywords) == 1:
+        where_clause = "keyword1 LIKE ? OR keyword2 LIKE ? OR keyword3 LIKE ?"
+        params = [*keywords[:]] * 3
+    elif len(keywords) == 2:
+        where_clause = "keyword1 LIKE ? AND keyword2 LIKE ? OR \
+            keyword1 LIKE ? AND keyword3 LIKE ? OR \
+            keyword2 LIKE ? AND keyword1 LIKE ? OR \
+            keyword2 LIKE ? AND keyword3 LIKE ? OR \
+            keyword3 LIKE ? AND keyword1 LIKE ? OR \
+            keyword3 LIKE ? AND keyword2 LIKE ?"
+        params = [*keywords[:]] * 6
+    else:
+        where_clause = "keyword1 LIKE ? AND keyword2 LIKE ? AND keyword3 LIKE ? OR \
+            keyword1 LIKE ? AND keyword3 LIKE ? AND keyword2 LIKE ? OR \
+            keyword2 LIKE ? AND keyword1 LIKE ? AND keyword3 LIKE ? OR \
+            keyword2 LIKE ? AND keyword3 LIKE ? AND keyword1 LIKE ? OR \
+            keyword3 LIKE ? AND keyword1 LIKE ? AND keyword2 LIKE ? OR \
+            keyword3 LIKE ? AND keyword2 LIKE ? AND keyword2 LIKE ?"
+        params = [*keywords[:]] * 6
     print(params)
-    print([f'{kw}' for kw in params])
+    
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    # where_clause = " OR ".join(["keyword1 LIKE ? OR keyword2 LIKE ? OR keyword3 LIKE ?" for _ in keywords])
+    # print(where_clause)
+    # params = []
+    # for kw in keywords:
+    #     params.extend([kw, kw, kw])
+    #     conn = get_db_connection()
+    #     cur = conn.cursor()
+    # print(params)
+    # print([f'{kw}' for kw in params])
 
     if query:
         cur.execute(f"""
@@ -37,13 +63,14 @@ def search():
         count = len(rows)
     return render_template('search.html', results=rows, count=count, query=query)
 
+
 @main.route('/site')
 def site():
-    # 여행지 클릭하면 해당 여행지 관련 블로그 포스트 보기로 제목에 하이퍼링크가 된 것들이 랜덤으로 20개 표시
+    # 여행지 클릭하면 해당 여행지 관련 블로그 포스트 보기로 제목에 하이퍼링크가 된 것들이 랜덤으로 10개 표시
     rows = []
 
     query = request.args.get('q', '')
-    
+
     conn = get_db_connection()
     cur = conn.cursor()
 
